8770366aae42feaa33c9d6633e08c6b250888bf7
mfqzhr <a584084434@outlook.com>
2019-11-15 08:45:22 +0800

    juc

---
+++ src/main/java/com/mfq/n2_1_1/ThreadA.java
@@ -0,0 +1,18 @@
+package com.mfq.n2_1_1;
+
+public class ThreadA extends Thread {
+	private HasSelfPrivateNum hasSelfPrivateNum;
+
+	public ThreadA(HasSelfPrivateNum hasSelfPrivateNum) {
+		super();
+		this.hasSelfPrivateNum = hasSelfPrivateNum;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+		hasSelfPrivateNum.addI("a");
+	}
+
+}
---
+++ src/main/java/com/mfq/n2_1_1/ThreadB.java
@@ -0,0 +1,18 @@
+package com.mfq.n2_1_1;
+
+public class ThreadB extends Thread {
+	private HasSelfPrivateNum hasSelfPrivateNum;
+
+	public ThreadB(HasSelfPrivateNum hasSelfPrivateNum) {
+		super();
+		this.hasSelfPrivateNum = hasSelfPrivateNum;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+		hasSelfPrivateNum.addI("b");
+	}
+
+}
